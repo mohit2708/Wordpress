8eaaa58032050f8241360c7b682ea66ec0102e29
Mohit Saxena <42208944+mohit2708@users.noreply.github.com>
2021-08-24 18:29:46 +0530

    Create add_admin_menu.php

---
+++ add_admin_menu.php
@@ -0,0 +1,24 @@
+```php
+add_action( 'admin_menu', 'my_admin_menu' );
+
+function my_admin_menu() {
+	add_menu_page( 'My Top Level Menu Example', 'Top Level Menu', 'manage_options', 'myplugin/myplugin-admin-page.php', 'myplguin_admin_page', 'dashicons-tickets', 6  );
+	add_submenu_page( 'myplugin/myplugin-admin-page.php', 'My Sub Level Menu Example', 'Sub Level Menu', 'manage_options', 'myplugin/myplugin-admin-sub-page.php', 'myplguin_admin_sub_page' ); 
+
+
+	add_submenu_page( 'myplugin/myplugin-admin-page.php', 'My Sub Level Menu Example', 'Sub Level Menu1', 'manage_options', 'myplugin-admin-sub-page.php', 'myplguin_admin_sub_page' ); 
+}
+function myplguin_admin_page(){
+	?>
+	<div class="wrap">
+		<h2>Welcome To My Plugin</h2>
+	</div>
+	<?php
+}function myplguin_admin_sub_page(){
+	?>
+	<div class="wrap">
+		<h2>Welcome To My Plugin</h2>
+	</div>
+	<?php
+}
+```
